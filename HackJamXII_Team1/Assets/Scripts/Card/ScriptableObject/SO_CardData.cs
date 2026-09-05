8c0ad968c950aa039116e0bddf4699c62183ed5c
Rubén Besteiro <rbesteiro@proton.me>
2026-09-05 15:08:04 +0200

    Cartas

--- HackJamXII_Team1/Assets/Scripts/Card/ScriptableObject/SO_CardData.cs
+++ HackJamXII_Team1/Assets/Scripts/Card/ScriptableObject/SO_CardData.cs
@@ -11,12 +11,12 @@ public class SO_CardData : ScriptableObject
     public string cardRightChoiceText;
     public string cardLeftChoiceText;
     public CardCategory cardCategory;
-    [Header("Card Values Right")]
-    public int cardGasValueRight;
-    public int cardTireValueRight;
-    public int cardChasisValueRight;
     [Header("Card Values Left")]
     public int cardGasValueLeft;
     public int cardTireValueLeft;
     public int cardChasisValueLeft;
+    [Header("Card Values Right")]
+    public int cardGasValueRight;
+    public int cardTireValueRight;
+    public int cardChasisValueRight;
 }
